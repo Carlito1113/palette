4c22f8504f5977e298583dbeb116980b511199e5
Carlito Algarin <carlito1113@yahoo.com>
2021-10-05 19:19:59 -0400

    added in mui snackbar and styled the select

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
-import { Select, MenuItem } from '@material-ui/core'
+// MUI - MATERIAL
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import Snackbar from '@mui/material/Snackbar'
+import IconButton from '@mui/material/IconButton'
+// MUI - ICONS
+import CloseIcon from '@mui/icons-material/Close'
+
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import '../styles/Navbar.css'
 
 const Navbar = ({ level, changeLevel, handleChange }) => {
   const [format, setFormat] = useState('hex')
+  const [open, setOpen] = useState(false)
 
-  const handleFormat = (e) => {
+  const handleFormatChange = e => {
     setFormat(e.target.value)
+    setOpen(true)
     handleChange(e.target.value)
+  }
+
+  const closeSnackbar = () => {
+    setOpen(false)
   }
 
   return (
@@ -18,7 +31,7 @@ const Navbar = ({ level, changeLevel, handleChange }) => {
         <a href="#">Palette.</a>
       </div>
       <div className="slider-container">
-      <span>Level: {level}</span>
+        <span>Level: {level}</span>
         <div className="slider">
           <Slider
             defaultValue={level}
@@ -32,12 +45,32 @@ const Navbar = ({ level, changeLevel, handleChange }) => {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={handleFormat}>
-            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
-            <MenuItem value='rgb'>RGB - rgb(255, 255, 255)</MenuItem>
-            <MenuItem value='rgba'>RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
+        <Select value={format} onChange={handleFormatChange}>
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'middle' }}
+        open={open}
+        autoHideDuration={3000}
+        message={<span id="message-id">Format Changed To {format}</span>}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </nav>
   )
 }
