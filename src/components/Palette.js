a3833516c950b9001aeab335d4356922c8b8b1f2
Carlito Algarin <carlito1113@yahoo.com>
2021-10-05 14:01:30 -0400

    added a slider to pick different palette shades

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ColorBox from './ColorBox'
 import '../styles/Palette.css'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 
-const Palette = ({ colors }) => {
-  const colorBoxes = colors.map(color => {
-    return <ColorBox background={color.color} name={color.name} />
+const Palette = ({ palette }) => {
+  const [level, setLevel] = useState(500)
+
+  const colorBoxes = palette.colors[level].map(color => {
+    return <ColorBox background={color.hex} name={color.name} />
   })
 
   return (
     <div className="Palette">
+      <Slider
+        defaultValue={level}
+        min={100}
+        max={900}
+        step={100}
+        onChange={value => {
+          setLevel(value)
+        }}
+      />
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
   )
